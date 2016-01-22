8b32d3916c72a646e30c1d2582b482e2a4af70c5
Sorin <trimbitassorin@hotmail.com>
2016-01-22 17:10:07 +0200

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -4,14 +4,6 @@
  * Email: trimbitassorin@hotmail.com
  */
 
-/**
- * Docs:
- * 	github for the mqtt class
- * 	http://blog.telenor.io/2015/10/15/lora-aws.html
- *  http://docs.aws.amazon.com/iot/latest/developerguide/protocols.html
- *
- */
-
 // Validate parameters
 if ( ! isset($_GET['topic']) OR ! isset($_GET['msg']))
 {
@@ -29,7 +21,7 @@ require_once(__DIR__.'/lib/mqtt.lib.php');
 
 $config = [
 	'aws_iot_host'          	=> 'A323VJS9G76W0W.iot.eu-west-1.amazonaws.com',
-	'aws_iot_port'				=> 8883,
+	'aws_iot_port'			=> 8883,
 	'aws_iot_cafile'        	=> __DIR__.'/certs/VeriSign-Class-3-Public-Primary-Certification-Authority-G5.pem',
 	'aws_iot_crtfile'       	=> __DIR__.'/certs/7e54b41b6a-certificate.pem.crt',
 	'aws_iot_private_keyfile'	=> __DIR__.'/certs/7e54b41b6a-private.pem.key',
